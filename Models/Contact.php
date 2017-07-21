67c541b26f542d3261ea276d35f9ec59bdcc8ae8
yKicchan <yKicchanApp@gmail.com>
2017-07-21 23:33:01 +0900

    clean:[#1] 改行、コメントの追加

--- Models/Contact.php
+++ Models/Contact.php
@@ -1,24 +1,30 @@
 <?php
 /**
+ * コンタクト表を扱うクラス
  *
+ * @package     starport
+ * @subpackage  Models
+ * @author      yKicchan
  */
 class Contact extends AppModel
 {
     /**
      * コンタクト情報を取得する
+     * 
      * @param  integer $lessonId レッスンID
      * @param  integer $userId   ユーザID
      * @return void
      */
     public function get($lessonId, $userId)
     {
-        $sql = "SELECT * FROM contact WHERE lesson_id = $lessonId AND user_id = $userId";
+        $sql = "SELECT * FROM `contact` WHERE `lesson_id` = $lessonId AND `user_id` = $userId";
         $row = $this->query($sql);
         return $row[0];
     }
 
     /**
      * コンタクト履歴があるかどうかを判定する
+     *
      * @param  integer  $lessonId レッスンID
      * @param  integer  $userId   ユーザID
      * @return boolean            true or false
