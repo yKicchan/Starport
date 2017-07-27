9fb4bb0856407393b90d039d98d664ddeb7dfe40
yKicchan <yKicchanApp@gmail.com>
2017-07-27 15:26:08 +0900

    update:[#1] ページ完成までコメントアウト
    設定ページが用意でき次第復活させる

--- Controllers/SettingController.php
+++ Controllers/SettingController.php
@@ -26,57 +26,57 @@ class SettingController extends AppController
         $this->disp('/Setting/profile.php');
     }
 
-    /**
-     * 作成したレッスンが一覧表示されるページ
-     * レッスンを選択して編集ページにジャンプできる
-     *
-     * @return void
-     */
-    public function lessonAction()
-    {
-        // ログインチェック
-        if (!$this->checkLoginStatus()) {
-            return;
-        }
+    // /**
+    //  * 作成したレッスンが一覧表示されるページ
+    //  * レッスンを選択して編集ページにジャンプできる
+    //  *
+    //  * @return void
+    //  */
+    // public function lessonAction()
+    // {
+    //     // ログインチェック
+    //     if (!$this->checkLoginStatus()) {
+    //         return;
+    //     }
+    //
+    //     // 作成されたレッスンの一覧を表示
+    //     $model = new Lesson();
+    //     $lesson = $model->getByUser($_SESSION['user_id']);
+    //     foreach ($lesson as &$val) {
+    //         $val['name']  = h($val['name']);
+    //         $val['about'] = h($val['about']);
+    //     }
+    //     $this->set('lesson', $lesson);
+    //     $this->disp("/Setting/lesson.php");
+    // }
 
-        // 作成されたレッスンの一覧を表示
-        $model = new Lesson();
-        $lesson = $model->getByUser($_SESSION['user_id']);
-        foreach ($lesson as &$val) {
-            $val['name']  = h($val['name']);
-            $val['about'] = h($val['about']);
-        }
-        $this->set('lesson', $lesson);
-        $this->disp("/Setting/lesson.php");
-    }
+    // /**
+    //  * ユーザのアカウントの設定ができるページ
+    //  * メールアドレスなどの個人情報はここで設定できる
+    //  *
+    //  * @return void
+    //  */
+    // public function accountAction()
+    // {
+    //     // ログインチェック
+    //     if (!$this->checkLoginStatus()) {
+    //         return;
+    //     }
+    //     $this->disp('/Setting/account.php');
+    // }
 
-    /**
-     * ユーザのアカウントの設定ができるページ
-     * メールアドレスなどの個人情報はここで設定できる
-     *
-     * @return void
-     */
-    public function accountAction()
-    {
-        // ログインチェック
-        if (!$this->checkLoginStatus()) {
-            return;
-        }
-        $this->disp('/Setting/account.php');
-    }
-
-    /**
-     * 外部アプリケーションの設定ができるページ
-     * TwitterやInstagramの連携・解除はここでできる予定
-     *
-     * @return void
-     */
-    public function applicationAction()
-    {
-        // ログインチェック
-        if (!$this->checkLoginStatus()) {
-            return;
-        }
-        $this->disp('/Setting/application.php');
-    }
+    // /**
+    //  * 外部アプリケーションの設定ができるページ
+    //  * TwitterやInstagramの連携・解除はここでできる予定
+    //  *
+    //  * @return void
+    //  */
+    // public function applicationAction()
+    // {
+    //     // ログインチェック
+    //     if (!$this->checkLoginStatus()) {
+    //         return;
+    //     }
+    //     $this->disp('/Setting/application.php');
+    // }
 }
