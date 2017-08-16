9f8afca19b8e0dbf52f833749e9bdd891851569a
yKicchan <yKicchanApp@gmail.com>
2017-08-16 15:43:03 +0900

    update:[#13] 必須・任意ラベルの設置

--- Views/Setting/profile.php
+++ Views/Setting/profile.php
@@ -3,7 +3,7 @@
         <h2>プロフィール編集</h2>
         <form method="post" action="/user/profile/<?= $_SESSION['user_id'] ?>">
             <div class="form-group">
-                <div class="form-label">氏名</div>
+                <div class="form-label">氏名<div class="form-required">必須</div></div>
                 <div class="inline">
                     <input type="text" class="form-control form148" name="data[last_name]" value="<?= $data['user']['last_name'] ?>"　required>
                 </div>
@@ -12,25 +12,25 @@
                 </div>
             </div>
             <div class="form-group">
-                <div class="form-label">メールアドレス</div>
+                <div class="form-label">メールアドレス<div class="form-required">必須</div></div>
                 <div class="inline">
                     <input type="email" class="form-control form300" name="data[email]" placeholder="例) info@starport.com" value="<?= $data['user']['email'] ?>" required>
                 </div>
             </div>
             <div class="form-group">
-                <div class="form-label">学校名</div>
+                <div class="form-label">学校名<div class="form-required">必須</div></div>
                 <div class="inline">
                     <input type="text" class="form-control form300" name="data[university]" placeholder="例) 京都大学" value="<?= $data['user']['university'] ?>" required>
                 </div>
             </div>
             <div class="form-group">
-                <div class="form-label">学部・学科名</div>
+                <div class="form-label">学部・学科名<div class="form-optional">任意</div></div>
                 <div class="inline">
                     <input type="text" class="form-control form300" name="data[faculty]" placeholder="例) 工学部" value="<?= $data['user']['faculty'] ?>">
                 </div>
             </div>
             <div class="form-group">
-                <div class="form-label">自己紹介文</div>
+                <div class="form-label">自己紹介文<div class="form-optional">任意</div></div>
                 <div class="inline">
                     <textarea class="form-control form300" name="data[introduction]" placeholder="例) テニスサークル所属、趣味は筋トレと映画鑑賞です。&#13;&#10;よろしくお願いします!!" ><?= $data['user']['introduction'] ?></textarea>
                 </div>
